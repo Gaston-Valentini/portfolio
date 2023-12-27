18293a9b412dad504b3d11a0bf95b88db2e2d1a3
Gastón <gastonvalentiniruiz@gmail.com>
2023-12-27 10:48:46 +0100

    Add: Navbar component added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import "./App.css";
+import style from "./App.module.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-    return <div>App</div>;
+    return (
+        <div className={style.app}>
+            <Navbar />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import style from "./Navbar.module.css";
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
+
+export default function Navbar() {
+    const [showLinks, setShowLinks] = useState(false);
+
+    const onBars = () => {
+        setShowLinks(!showLinks);
+    };
+
+    return (
+        <nav className={style.container}>
+            <h1 className={style.logo}>GASTON</h1>
+            <div className={style.bars}>
+                <FaBars className={style.barsIcon} onClick={onBars} />
+            </div>
+            <ul className={`${style.links} ${showLinks ? style.linksOpen : ""}`}>
+                <li className={style.linksLink}>
+                    <a href="#">Inicio</a>
+                </li>
+                <li className={style.linksLink}>
+                    <a href="#">Sobre mi</a>
+                </li>
+                <li className={style.linksLink}>
+                    <a href="#">Proyectos</a>
+                </li>
+                <li className={style.linksLink}>
+                    <a href="#">Tecnologías</a>
+                </li>
+                <li className={style.linksLink}>
+                    <a href="#">Contacto</a>
+                </li>
+            </ul>
+        </nav>
+    );
+}

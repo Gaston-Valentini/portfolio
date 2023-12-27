d1541adeb01454d68f5bd625f40d334639e033a7
Gastón <gastonvalentiniruiz@gmail.com>
2023-12-27 11:57:20 +0100

    Add: Header component added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import style from "./App.module.css";
 import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header/Header";
 
 function App() {
     return (
         <div className={style.app}>
             <Navbar />
+            <Header />
         </div>
     );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import style from "./Header.module.css";
+
+export default function Header() {
+    return (
+        <header className={style.container}>
+            <div className={style.info}>
+                <h2 className={style.infoHello}>Hola, mi nombre es...</h2>
+                <h1 className={style.infoName}>Gastón Valentini</h1>
+                <h1 className={style.infoOccupation}>
+                    <span>___ </span>Full Stack Web Developer
+                </h1>
+                <button className={style.infoDownload}>Descargar CV</button>
+            </div>
+        </header>
+    );
+}

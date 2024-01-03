6fd0059041134ec2f988f05d19e97a7110ca3ac0
Gastón <gastonvalentiniruiz@gmail.com>
2024-01-03 14:42:51 +0100

    Add: Contact component added.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Technologies from "./components/Technologies/Technologies";
+import Contact from "./components/Contact/Contact";
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
             <About />
             <Projects />
             <Technologies />
+            <Contact />
         </div>
     );
 }
